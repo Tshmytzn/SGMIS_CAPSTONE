567052c6506e8208e430f9b3cc3ba8c74b11ffb2
jpubas <jpubas@gmail.com>
2024-05-10 22:42:12 +0800

    update cdn

---
+++ app/Http/Controllers/DeparmentData.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Department;
+
+class DeparmentData extends Controller
+{
+   public function SavaDepartment(Request $request){
+
+    
+
+   }
+}
--- resources/views/Admin/components/header.blade.php
+++ resources/views/Admin/components/header.blade.php
@@ -1,26 +1,33 @@
 <head>
-    <meta charset="utf-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover"/>
-    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
-    <title> {{$title}}</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title> {{ $title }}</title>
 
     <!-- Favicon -->
-    <link href="{{asset('./static/logoicon.png')}}" rel="icon">
+    <link href="{{ asset('./static/logoicon.png') }}" rel="icon">
 
-    <link href="{{asset('./dist/css/tabler.min.css?1684106062')}}" rel="stylesheet"/>
-    <link href="{{asset('./dist/css/tabler-flags.min.css?1684106062')}}" rel="stylesheet"/>
-    <link href="{{asset('./dist/css/tabler-payments.min.css?1684106062')}}" rel="stylesheet"/>
-    <link href="{{asset('./dist/css/tabler-vendors.min.css?1684106062')}}" rel="stylesheet"/>
-    <link href="{{asset('./dist/css/demo.min.css?1684106062')}}" rel="stylesheet"/>
+    <link href="{{ asset('./dist/css/tabler.min.css?1684106062') }}" rel="stylesheet" />
+    <link href="{{ asset('./dist/css/tabler-flags.min.css?1684106062') }}" rel="stylesheet" />
+    <link href="{{ asset('./dist/css/tabler-payments.min.css?1684106062') }}" rel="stylesheet" />
+    <link href="{{ asset('./dist/css/tabler-vendors.min.css?1684106062') }}" rel="stylesheet" />
+    <link href="{{ asset('./dist/css/demo.min.css?1684106062') }}" rel="stylesheet" />
+
+    {{-- new CDN --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.slim.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.slim.min.js"></script>
+
     <style>
+        @import url('https://rsms.me/inter/inter.css');
 
-    @import url('https://rsms.me/inter/inter.css');
-      :root {
-      	--tblr-font-sans-serif: 'Inter Var', -apple-system, BlinkMacSystemFont, San Francisco, Segoe UI, Roboto, Helvetica Neue, sans-serif;
-    }
-      body {
-      	font-feature-settings: "cv03", "cv04", "cv11";
-    }
+        :root {
+            --tblr-font-sans-serif: 'Inter Var', -apple-system, BlinkMacSystemFont, San Francisco, Segoe UI, Roboto, Helvetica Neue, sans-serif;
+        }
 
+        body {
+            font-feature-settings: "cv03", "cv04", "cv11";
+        }
     </style>
 </head>
