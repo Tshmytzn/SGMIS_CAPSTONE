583307a01f6e082a96b238cde9c56580119bcdb1
Tshmytzn <128598268+Tshmytzn@users.noreply.github.com>
2024-05-16 17:25:05 +0800

    added card to evaluation

--- resources/views/Admin/evaluation.blade.php
+++ resources/views/Admin/evaluation.blade.php
@@ -54,6 +54,17 @@
           <div class="container-xl">
             <div class="row row-deck row-cards">
               
+              <div class="col-md-6 col-lg-3">
+                <div class="card">
+                  <!-- Photo -->
+                  <div class="img-responsive img-responsive-21x9 card-img-top" style="background-image: url(./static/photos/home-office-desk-with-macbook-iphone-calendar-watch-and-organizer.jpg)"></div>
+                  <div class="card-body">
+                    <h3 class="card-title">Card with top image</h3>
+                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti fugit incidunt, iste, itaque minima
+                      neque pariatur perferendis sed suscipit velit vitae voluptatem.</p>
+                  </div>
+                </div>
+              </div>
 
             </div>
           </div>
