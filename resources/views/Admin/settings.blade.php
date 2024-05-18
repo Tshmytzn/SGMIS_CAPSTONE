d31cf013dcc92b34617e96c0fb806f590293bfed
Tshmytzn <128598268+Tshmytzn@users.noreply.github.com>
2024-05-18 14:05:43 +0800

    ADDED STUDENT TAB IN SETTINGS

--- resources/views/Admin/settings.blade.php
+++ resources/views/Admin/settings.blade.php
@@ -27,7 +27,10 @@
                         <a class="nav-link active" id="my-account-tab" data-bs-toggle="pill" href="#my-account"><h3>My Account</h3></a>
                       </li>
                       <li class="nav-item">
-                        <a class="nav-link" id="administrators-tab" data-bs-toggle="pill" href="#administrators"><h3>Administrators</h3></a>
+                        <a class="nav-link" id="administrators-tab" data-bs-toggle="pill" href="#administrators"><h3>Primary Admins</h3></a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" id="studentadmins-tab" data-bs-toggle="pill" href="#studentadmins"><h3>Student Admins</h3></a>
                       </li>
                     </ul>
                   </div>
@@ -138,6 +141,104 @@
                       
                         
                         
+                      </div>
+                                            {{-- administrator --}}
+                      <div class="tab-pane fade" id="administrators">
+                      <div class="d-flex align-items-center justify-content-end mb-3">
+
+                        <div class="input-icon">
+                            <input type="text" value="" class="form-control" placeholder="Search…">
+                            <span class="input-icon-addon">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" /><path d="M21 21l-6 -6" /></svg>
+                            </span>
+                        </div> &nbsp; &nbsp;
+                        <button class="btn btn-primary me-3" data-bs-toggle="modal" data-bs-target="#addnewadmin">Add New Administrator</button>
+
+                    </div>
+                        
+                        <div class="row g-5" id="adminCard">
+
+                          {{-- <div id="administrators-card" class="col-md-6 col-lg-4 admincardeffects">
+                            <div class="card">
+                              <div class="card-body p-4 text-center">
+                                <span class="avatar avatar-xl mb-3 rounded" style="background-image: url({{asset('./static/avatars/002m.jpg')}})"><img src="" alt=""> </span>
+                                <h3 class="m-0 mb-1"><a href="#">Ghiza Ann Dojoles</a></h3>
+                                <div class="text-muted">SSG Secretary</div>
+                                <div class="mt-3">
+                                  <span class="badge bg-green-lt">Administrator</span>
+                                </div>
+                              </div>
+                              <div class="d-flex">
+                                <a href="#" data-bs-toggle="modal" data-bs-target="#editadmin" class="card-btn">
+                                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-edit">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                    <path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1"/>
+                                    <path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z"/>
+                                    <path d="M16 5l3 3"/>
+                                  </svg>                                
+                                  &nbsp; Edit
+                                </a>
+                                <a href="#" data-bs-toggle="modal" data-bs-target="#demotemodal" class="card-btn">
+                                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-arrow-down-to-arc">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                    <path d="M12 3v12"/>
+                                    <path d="M16 11l-4 4l-4 -4"/>
+                                    <path d="M3 12a9 9 0 0 0 18 0"/>
+                                  </svg>
+                                  &nbsp; Demote
+                                </a>
+                              </div>
+                            </div>
+                          </div> --}} 
+                      </div>
+                      </div>
+                      {{-- student administrator --}}
+                      <div class="tab-pane fade" id="studentadmins">
+                        <div class="d-flex align-items-center justify-content-end mb-3">
+                          <div class="input-icon">
+                              <input type="text" value="" class="form-control" placeholder="Search…">
+                              <span class="input-icon-addon">
+                                  <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" /><path d="M21 21l-6 -6" /></svg>
+                              </span>
+                          </div> &nbsp; &nbsp;
+                          <button class="btn btn-primary me-3" data-bs-toggle="modal" data-bs-target="#addstudentadmin">Add New Student Admin</button>
+                      </div>
+
+                        <div class="row g-5" id="adminCard">
+                          <div id="administrators-card" class="col-md-6 col-lg-4 admincardeffects">
+                            <div class="card">
+                              <div class="card-body p-4 text-center">
+                                <span class="avatar avatar-xl mb-3 rounded" style="background-image: url({{asset('./static/avatars/002m.jpg')}})"><img src="" alt=""> </span>
+                                <h3 class="m-0 mb-1"><a href="#">Ghiza Ann Dojoles</a></h3>
+                                <div class="text-muted">SSG Secretary</div>
+                                <div class="mt-3">
+                                  <span class="badge bg-green-lt">Administrator</span>
+                                </div>
+                              </div>
+                              <div class="d-flex">
+                                <a href="#" data-bs-toggle="modal" data-bs-target="#editstudentadmin" class="card-btn">
+                                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-edit">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                    <path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1"/>
+                                    <path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z"/>
+                                    <path d="M16 5l3 3"/>
+                                  </svg>                                
+                                  &nbsp; Edit
+                                </a>
+                                <a href="#" data-bs-toggle="modal" data-bs-target="#studentdemote" class="card-btn">
+                                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-arrow-down-to-arc">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                    <path d="M12 3v12"/>
+                                    <path d="M16 11l-4 4l-4 -4"/>
+                                    <path d="M3 12a9 9 0 0 0 18 0"/>
+                                  </svg>
+                                  &nbsp; Demote
+                                </a>
+                              </div>
+                            </div>
+                          </div>
+
+                        </div>
                       </div>
                     </div>
                   </div>
@@ -322,6 +423,102 @@
                           </div>
                         </div>
                         {{-- upload profile pic --}} 
+
+        {{-- Add Student Modal --}}
+        <div class="modal modal-blur fade" id="addstudentadmin" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header text-white" style="background-color: #3E8A34;">
+                <h5 class="modal-title" id="staticBackdropLabel">Add Student Admin</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+
+                <div class="table-responsive">
+                  <table class="table table-bordered table-hover">
+                    <thead>
+                      <tr>
+                        <th>Full Name</th>
+                        <th>SCHOOL ID No.</th>
+                        <th>USG Position</th>
+                        <th>Actions</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>Sample name</td>
+                        <td>34545469</td>
+                        <td>USG PRESIDENT</td>
+                        <td>
+                          <button class="btn btn-warning btn-sm">Select</button>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+                <hr>
+                
+                <form class="row g-3" id="addnewstudentadmin" method="POST">@csrf
+
+                <div class="row g-2">
+                  <div class="col-12">
+                    <label for="adminname" class="form-label">Full Name</label>
+                    <input type="text" class="form-control" name="administratorname" id="administratorname">         
+                  </div>
+                  <div class="col-6">
+                    <label for="adminuser" class="form-label">SCHOOL ID No.</label>
+                      <input type="number" class="form-control" name="administratoruser" id="administratoruser">         
+                  </div>
+                  <div class="col-6">
+                    <label for="adminuser" class="form-label">USG Position</label>
+                      <select name="" class="form-select" id="">
+                        <option value=""> USG PRESIDENT </option>
+                        <option value=""> USG SECRETARY </option>
+                      </select>         
+                  </div>
+
+                </div>
+
+                </form>
+
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancel</button>
+                <button type="button" class="btn btn-primary" onclick="()">Add as Admin</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        {{-- Add Student Modal --}}
+
+                                {{-- demote modal --}}
+                                <div class="modal modal-blur fade" id="studentdemote" tabindex="-1" role="dialog" aria-hidden="true">
+                                  <div class="modal-dialog modal-sm modal-dialog-centered" role="document">
+                                    <div class="modal-content">
+                                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                      <div class="modal-status bg-danger"></div>
+                                      <div class="modal-body text-center py-4">
+                                        <!-- Download SVG icon from http://tabler-icons.io/i/alert-triangle -->
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon mb-2 text-danger icon-lg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10.24 3.957l-8.422 14.06a1.989 1.989 0 0 0 1.7 2.983h16.845a1.989 1.989 0 0 0 1.7 -2.983l-8.423 -14.06a1.989 1.989 0 0 0 -3.4 0z" /><path d="M12 9v4" /><path d="M12 17h.01" /></svg>
+                                        <h3>Confirm Demotion</h3>
+                                        <div class="text-muted">Are you sure you want to demote this Student admin and revoke their admin privileges?</div>
+                                      </div>
+                                      <div class="modal-footer">
+                                        <div class="w-100">
+                                          <div class="row">
+                                            <div class="col"><a href="#" class="btn w-100" data-bs-dismiss="modal">
+                                                Cancel
+                                              </a></div>
+                                            <div class="col"><a href="#" class="btn btn-danger w-100" data-bs-dismiss="modal">
+                                                Yes, Demote
+                                              </a></div>
+                                          </div>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                                {{-- demote modal --}}
 
         {{-- Modal --}}
        
