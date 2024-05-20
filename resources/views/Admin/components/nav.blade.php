a6f45cba6f2df4ee2b7c52ae6071b94828eb209f
Tshmytzn <128598268+Tshmytzn@users.noreply.github.com>
2024-05-20 11:45:10 +0800

    added new page for evaluations and compendium.

--- resources/views/Admin/components/nav.blade.php
+++ resources/views/Admin/components/nav.blade.php
@@ -216,7 +216,7 @@
                       </span>
                     </a>
                   </li>
-
+{{-- 
                   <li class="nav-item dropdown {{$active == 'Events' ? 'active' : '' }}">
                     <a class="nav-link" href="{{route('Events')}}" >
                       <span class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/ghost -->
@@ -225,7 +225,25 @@
                         Events
                       </span>
                     </a>
-                  </li>
+                  </li> --}}
+
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#navbar-help" data-bs-toggle="dropdown" data-bs-auto-close="outside" role="button" aria-expanded="false" >
+                    <span class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/lifebuoy -->
+                      <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-calendar-event"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 5m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" /><path d="M16 3l0 4" /><path d="M8 3l0 4" /><path d="M4 11l16 0" /><path d="M8 15h2v2h-2z" /></svg>                      </span>
+                    </span>
+                    <span class="nav-link-title">
+                      Events
+                    </span>
+                  </a>
+                  <div class="dropdown-menu">
+                    <a class="dropdown-item" href="" target="_blank" rel="noopener">
+                      <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-checklist"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9.615 20h-2.615a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h8a2 2 0 0 1 2 2v8" /><path d="M14 19l2 2l4 -4" /><path d="M9 8h4" /><path d="M9 12h2" /></svg>                      
+                      &nbsp; Attendance
+                    </a>
+                  </div>
+                </li>
+                  
                   <li class="nav-item {{$active == 'Evaluation' ? 'active' : '' }}">
                     <a class="nav-link" href="{{route('Evaluation')}}" >
                       <span class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/ghost -->
---
+++ resources/views/Admin/viewevaluations.blade.php
@@ -0,0 +1,55 @@
+<!doctype html>
+
+<html lang="en">
+  
+@include('Admin.components.header', ['title' => 'Evaluation Details'])
+
+  <body >
+    <script src="{{asset('./dist/js/demo-theme.min.js?1684106062')}}"></script>
+
+    <div class="page">
+
+@include('Admin.components.nav', ['active' => ''])
+
+            <div class="page-wrapper">
+
+        <!-- Page header -->
+        <div class="page-header d-print-none">
+          <div class="container-xl">
+            <div class="row g-2 align-items-center">
+              <div class="col">
+
+                <!-- Page pre-title -->
+                <div class="page-pretitle">
+                  Overview
+                </div>
+                <h2 class="page-title">
+                  Evaluation details
+                </h2>
+              </div>
+              
+
+            </div>
+          </div>
+        </div>
+        
+        <!-- Page body -->
+        <div class="page-body">
+          <div class="container-xl">
+            <div class="row row-deck row-cards">
+              
+
+            </div>
+          </div>
+        </div>
+
+@include('Admin.components.footer')
+
+      </div>
+    </div>
+    
+    
+@include('Admin.components.scripts')
+
+  </body>
+</html>
