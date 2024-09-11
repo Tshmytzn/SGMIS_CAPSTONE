2b07a3e3bcdd18f8049dbd9a2e84358e970afc30
Tisha May Tizon <128598268+Tshmytzn@users.noreply.github.com>
2024-09-11 18:30:02 +0800

    Updated admin login: view password

--- resources/views/Admin/login.blade.php
+++ resources/views/Admin/login.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 
 <html lang="en">
-  
+
 @include('Admin.components.header', ['title' => 'Login'])
 
   <body  class=" d-flex flex-column">
@@ -35,7 +35,6 @@
                         <input type="password" class="form-control" name="password" id="password"  placeholder="Your password"  autocomplete="off">
                         <span class="input-group-text">
                           <a href="#" class="link-secondary" title="Show password" data-bs-toggle="tooltip"><!-- Download SVG icon from http://tabler-icons.io/i/eye -->
-                            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" /><path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" /></svg>
                           </a>
                         </span>
                       </div>
