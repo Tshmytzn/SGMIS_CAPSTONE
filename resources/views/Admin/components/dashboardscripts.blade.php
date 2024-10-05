c0ca721ba8ee0f906fbd332e4b07ae1e6d58c739
jpubas <jpubas@gmail.com>
2024-10-05 21:50:52 +0800

    update sg

--- resources/views/Admin/components/dashboardscripts.blade.php
+++ resources/views/Admin/components/dashboardscripts.blade.php
@@ -1,6 +1,14 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<script src="{{ asset('js/evo-calendar.js') }}"></script>
 
+
+
+<script src="{{ asset('./dist/js/tabler.min.js?1684106062') }}" defer></script>
+<script src="{{ asset('./dist/js/demo.min.js?1684106062') }}" defer></script>
+<script src="{{asset('./dist/libs/nouislider/dist/nouislider.min.js?1684106062')}}" defer></script>
+<script src="{{asset('./dist/libs/litepicker/dist/litepicker.js?1684106062')}}" defer></script>
+<script src="{{asset('./dist/libs/tom-select/dist/js/tom-select.base.min.js?1684106062')}}" defer></script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="js/evo-calendar.js"></script>
 <script>
     $(document).ready(function() {
         $("#calendar").evoCalendar({
@@ -25,9 +33,3 @@
         });
     });
 </script>
-
-<script src="{{ asset('./dist/js/tabler.min.js?1684106062') }}" defer></script>
-<script src="{{ asset('./dist/js/demo.min.js?1684106062') }}" defer></script>
-<script src="{{asset('./dist/libs/nouislider/dist/nouislider.min.js?1684106062')}}" defer></script>
-<script src="{{asset('./dist/libs/litepicker/dist/litepicker.js?1684106062')}}" defer></script>
-<script src="{{asset('./dist/libs/tom-select/dist/js/tom-select.base.min.js?1684106062')}}" defer></script>
