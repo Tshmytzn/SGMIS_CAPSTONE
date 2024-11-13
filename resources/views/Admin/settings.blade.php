1c110c1707e2065d756a2214b23e623ca691a791
Tish <tbtizon.chmsu@gmail.com>
2024-11-13 22:42:23 +0800

    fixed modal alert

--- resources/views/Admin/settings.blade.php
+++ resources/views/Admin/settings.blade.php
@@ -438,8 +438,8 @@
                               <div class="modal-body text-center py-4">
                                 <!-- Download SVG icon from http://tabler-icons.io/i/alert-triangle -->
                                 <svg xmlns="http://www.w3.org/2000/svg" class="icon mb-2 text-danger icon-lg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10.24 3.957l-8.422 14.06a1.989 1.989 0 0 0 1.7 2.983h16.845a1.989 1.989 0 0 0 1.7 -2.983l-8.423 -14.06a1.989 1.989 0 0 0 -3.4 0z" /><path d="M12 9v4" /><path d="M12 17h.01" /></svg>
-                                <h3>Confirm Demotion</h3>
-                                <div class="text-muted">Are you sure you want to demote this administrator and revoke their admin privileges?</div>
+                                <h3>Confirm Disabled</h3>
+                                <div class="text-muted">Are you sure you want to Disable this administrator and revoke their admin privileges?</div>
                                 <form action="" method="POST" id="demoteadminform">@csrf
                                   <input type="hidden" name="demoteadminid" id="demoteadminid">
                                 </form>
@@ -451,7 +451,7 @@
                                         Cancel
                                       </a></div>
                                     <div class="col"><a href="#" class="btn btn-danger w-100" data-bs-dismiss="modal" onclick="DemoteAdmin()">
-                                        Yes, Demote
+                                        Yes, Disable
                                       </a></div>
                                   </div>
                                 </div>
