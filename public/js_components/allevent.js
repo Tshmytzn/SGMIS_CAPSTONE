62fac9ffd981e73bfb687a940c803cc3ac4ff121
jpubas <jpubas@gmail.com>
2024-11-13 20:24:01 +0800

    update event

--- public/js_components/allevent.js
+++ public/js_components/allevent.js
@@ -332,11 +332,7 @@ function LoadEvents(route, imageRoute, deleteEvent, eventDetails, where) {
                 ${where == 'admin' ? ' Try adding event by clicking the add button below' : 'Opps there is no event that is currently published'}
         </p>
         ${where === 'admin' ? `<div class="empty-action">
-          <button data-bs-toggle="modal" data-bs-target="#modal-report" class="btn btn-primary">
-
-            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 5l0 14" /><path d="M5 12l14 0" /></svg>
-            Add Event
-          </button>
+         
         </div>` : ''}
     </div>`;
             }
