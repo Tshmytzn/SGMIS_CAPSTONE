cceb0f1c57448786b7171226aa47bf57976b6a6a
jpubas <jpubas@gmail.com>
2024-11-15 05:18:30 +0800

    update sg

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
 );
 
+$app->usePublicPath($app->basePath('public_html'));
 /*
 |--------------------------------------------------------------------------
 | Bind Important Interfaces
